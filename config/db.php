421e71683bba4d17ac9349b3163175598caebc36
federicomalvasi <federico.malvasi@applica.guru>
2021-08-09 09:06:13 +0200

    nuovi parametri db

--- config/db.php
+++ config/db.php
@@ -2,16 +2,18 @@
 
 return [
     'class' => 'yii\db\Connection',
-    //'dsn' => 'mysql:host=89.46.111.76;dbname=Sql1269611_1',
-    //'username' => 'Sql1269611',
-    //'password' => 'ry5727mi48',	
+//    'dsn' => 'mysql:host=31.11.39.18;dbname=Sql1498307_1',
+//    'username' => 'Sql1498307',
+//    'password' => '81w06zw7ez',
+    'charset' => 'utf8',
+
     'dsn' => 'mysql:host=127.0.0.1;dbname=circoscrizione',
 	'username' => 'root',
 	'password' => 'ciccio',
-    'charset' => 'utf8',
 
     // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+//    'enableSchemaCache' => true,
+//    'schemaCacheDuration' => 60,
+//    'schemaCache' => 'cache',
 ];
+
